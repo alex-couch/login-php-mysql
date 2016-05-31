3609bdad6942c9d37d593f17ee66e49071c4dddd
Alex <tempus.studios.contact@gmail.com>
2016-05-31 09:42:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Database and Login</title>
+	</head>
+	<body>
+		<form method="post" action="db.php">
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password" placeholder="Password">
+			<input type="text" name="email" placeholder="E-Mail">
+			<input type="submit" name="submit" value="submit">
+		</form>
+	</body>
+</html>
